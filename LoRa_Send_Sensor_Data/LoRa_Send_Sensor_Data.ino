8f9399d68911f79fca0492ff11ab1f8432c967b9
Bologne <berkeykaplan@gmail.com>
2022-02-28 18:33:19 -0600

    LoRa_Send Update

--- LoRa_Send_Sensor_Data/LoRa_Send_Sensor_Data.ino
+++ LoRa_Send_Sensor_Data/LoRa_Send_Sensor_Data.ino
@@ -5,6 +5,8 @@
 #include <string>
 #include "ICM_20948.h"
 #include <cmath>
+#include <SparkFun_u-blox_GNSS_Arduino_Library.h>
+
 #define RFM95_CS 8
 #define RFM95_RST 4
 #define RFM95_INT 3
@@ -23,7 +25,7 @@ BME280 myBME280; //Object to interface with the Sparkfun Environmental
 I2CGPS myI2CGPS; //Object to interface with the Sparkfun GPS
 TinyGPSPlus gps; //Declare gps object
 ICM_20948_I2C myICM;  // Object to interface with the Sparkfun IMU
-
+SFE_UBLOX_GNSS myGNSS;
 using namespace std;
 
 void setup() {
@@ -49,7 +51,7 @@ void setup() {
   WIRE_PORT.begin();
   WIRE_PORT.setClock(400000);
   myICM.begin( WIRE_PORT, AD0_VAL );
-  
+
   // Start the GPS sensor
   if (myI2CGPS.begin() == false) Serial.println("Module failed to respond. Please check wiring.");
 
@@ -58,22 +60,29 @@ void setup() {
 
   // Start the Environmental Sensor
   if (myBME280.beginI2C() == false) Serial.println("Could not connect to the environmental sensor for temperature.");
+
+  if (myGNSS.begin() == false) Serial.println(F("u-blox GNSS not detected at default I2C address. Please check wiring. Freezing."));
+  myGNSS.setI2COutput(COM_TYPE_UBX); //Set the I2C port to output UBX only (turn off NMEA noise)
+  myGNSS.saveConfigSelective(VAL_CFG_SUBSEC_IOPORT); //Save (only) the communications port settings to flash and BBR
 }
  
 void loop(){
   string message = "C848E70850553158342E3120FF19312E";
   
   // Temperature Portion
+  /*
   float BMEtempC = myBME280.readTempF();
   string BMEtempCstring= to_string(BMEtempC);
   message = message + ',' + BMEtempCstring.substr(0,BMEtempCstring.length() - 4);
   
   delay(10); //Don't spam the I2C bus
-  
+  */
+
+  /*
   // Obtain IMU information
   if( myICM.dataReady() ){
     myICM.getAGMT(); // The values are only updated when you call 'getAGMT'
-    message = message + ',' + printScaledAGMT(&myICM);
+    message = message + printScaledAGMT(&myICM); //',' + printScaledAGMT(&myICM);
     delay(30);
   }
   
@@ -89,16 +98,23 @@ void loop(){
   Serial.println(message.c_str());
   rf95.send((uint8_t*) message.c_str(), RH_RF95_MAX_MESSAGE_LEN);
   rf95.waitPacketSent();
+  */
+
+  double latitude = myGNSS.getLatitude()/10000000.0;
+  double longitude = myGNSS.getLongitude()/10000000.0;
+  message = message + ',' + to_string(latitude) + " " + to_string(longitude);
   
-  delay(100);
+  Serial.println(message.c_str());
+  rf95.send((uint8_t*) message.c_str(), RH_RF95_MAX_MESSAGE_LEN);
+  rf95.waitPacketSent();
+  delay(500);
+  
 }
 
 //Display new GPS info
 string GPSInfo(){
   string result = "";
-  if (gps.location.isValid()){
-    result = to_string(gps.location.lat()) + " " + to_string(gps.location.lng());
-  }
+  result = to_string(gps.location.lat()) + " " + to_string(gps.location.lng());
   return result;
 }
 
