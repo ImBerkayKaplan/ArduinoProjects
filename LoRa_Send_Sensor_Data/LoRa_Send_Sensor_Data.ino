937496b6ca463ee5f29aea5cb9c26158ef55575e
Bologne <berkeykaplan@gmail.com>
2022-03-02 15:09:20 -0600

    Integrated GNSS into send_sensor

--- LoRa_Send_Sensor_Data/LoRa_Send_Sensor_Data.ino
+++ LoRa_Send_Sensor_Data/LoRa_Send_Sensor_Data.ino
@@ -1,6 +1,4 @@
 #include <RH_RF95.h>
-#include "SparkFunBME280.h"
-#include "SparkFun_I2C_GPS_Arduino_Library.h"
 #include <TinyGPS++.h>
 #include <string>
 #include "ICM_20948.h"
@@ -10,7 +8,6 @@
 #define RFM95_CS 8
 #define RFM95_RST 4
 #define RFM95_INT 3
-#define CCS811_ADDR 0x5B
 #define WIRE_PORT Wire
 #define AD0_VAL   1
 
@@ -20,10 +17,6 @@
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
-
-BME280 myBME280; //Object to interface with the Sparkfun Environmental
-I2CGPS myI2CGPS; //Object to interface with the Sparkfun GPS
-TinyGPSPlus gps; //Declare gps object
 ICM_20948_I2C myICM;  // Object to interface with the Sparkfun IMU
 SFE_UBLOX_GNSS myGNSS;
 using namespace std;
@@ -52,14 +45,8 @@ void setup() {
   WIRE_PORT.setClock(400000);
   myICM.begin( WIRE_PORT, AD0_VAL );
 
-  // Start the GPS sensor
-  if (myI2CGPS.begin() == false) Serial.println("Module failed to respond. Please check wiring.");
-
   // Start the IMU sensor
   if( myICM.status != ICM_20948_Stat_Ok ) Serial.println( "Could not connect to the IMU sensor for 3D movement." );
-
-  // Start the Environmental Sensor
-  if (myBME280.beginI2C() == false) Serial.println("Could not connect to the environmental sensor for temperature.");
 
   if (myGNSS.begin() == false) Serial.println(F("u-blox GNSS not detected at default I2C address. Please check wiring. Freezing."));
   myGNSS.setI2COutput(COM_TYPE_UBX); //Set the I2C port to output UBX only (turn off NMEA noise)
@@ -69,15 +56,6 @@ void setup() {
 void loop(){
   string message = "C848E70850553158342E3120FF19312E";
   
-  // Temperature Portion
-  /*
-  float BMEtempC = myBME280.readTempF();
-  string BMEtempCstring= to_string(BMEtempC);
-  message = message + ',' + BMEtempCstring.substr(0,BMEtempCstring.length() - 4);
-  
-  delay(10); //Don't spam the I2C bus
-  */
-
   /*
   // Obtain IMU information
   if( myICM.dataReady() ){
@@ -85,23 +63,10 @@ void loop(){
     message = message + printScaledAGMT(&myICM); //',' + printScaledAGMT(&myICM);
     delay(30);
   }
-  
-  // Obtain GPS information
-  while (myI2CGPS.available()){
-    gps.encode(myI2CGPS.read()); //Feed the GPS parser
-  }
-  if (gps.time.isUpdated()){
-    message = message + ',' + GPSInfo();
-  }
-  
-  // Print the message and send it to the receiver Adafruit Feather through LoRa
-  Serial.println(message.c_str());
-  rf95.send((uint8_t*) message.c_str(), RH_RF95_MAX_MESSAGE_LEN);
-  rf95.waitPacketSent();
   */
 
-  double latitude = myGNSS.getLatitude()/10000000.0;
-  double longitude = myGNSS.getLongitude()/10000000.0;
+  double latitude = myGNSS.getLatitude()/10000000.;
+  double longitude = myGNSS.getLongitude()/10000000.;
   message = message + ',' + to_string(latitude) + " " + to_string(longitude);
   
   Serial.println(message.c_str());
@@ -109,13 +74,6 @@ void loop(){
   rf95.waitPacketSent();
   delay(500);
   
-}
-
-//Display new GPS info
-string GPSInfo(){
-  string result = "";
-  result = to_string(gps.location.lat()) + " " + to_string(gps.location.lng());
-  return result;
 }
 
 string printScaledAGMT(ICM_20948_I2C *sensor){
